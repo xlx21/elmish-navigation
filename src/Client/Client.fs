5fc604e8b7cec0258dbc038f4cc9164c8b3d4e90
Alican Demirtas <alican@compositional-it.com>
2020-04-17 18:16:45 +0100

    Some styling added.

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -10,8 +10,30 @@ open Elmish.Navigation
 
 module HomePage =
     type Model = { Title : string }
-    let init () = { Title = "Welcome! You're in HOME PAGE. " }
-    let view model dispatch = Content.content [] [ str model.Title ]
+
+    let init () = { Title = "Welcome! You're in the Home Page." }
+
+    let view (model: Model) dispatch =
+        div [] [
+            Content.content [
+                Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ]
+            ] [
+                Heading.h1 [] [ str model.Title ]
+            ]
+        ]
+
+module PersonPage =
+    type Model = { Name : string }
+
+    let init fullName = { Name = fullName }
+
+    let view model dispatch =
+        Content.content [
+            Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ]
+        ] [
+            Heading.h1 [ Heading.Option.Props [ Style [ Margin "2rem" ] ] ] [ str "Person Details" ]
+            Heading.h3 [ Heading.IsSubtitle ] [ str (sprintf "Full Name: %s" model.Name) ]
+        ]
 
 module AddressPage =
     type Model =
@@ -27,21 +49,14 @@ module AddressPage =
         Street = "Liverpool St" }
 
     let view model dispatch =
-        Content.content [] [
-            str "Address Page"
-            str (sprintf "Building No: %d" model.BuildingNo)
-            str (sprintf "Street: %s" model.Street)
-            str (sprintf "City: %s" model.City)
-            str (sprintf "Postcode: %s" model.Postcode)
-        ]
-
-module PersonPage =
-    type Model = { Name : string }
-    let init fullName = { Name = fullName }
-    let view model dispatch =
-        Content.content [] [
-            str "Person Details"
-            str (sprintf "Full Name: %s" model.Name)
+        Content.content [
+            Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ]
+        ] [
+            Heading.h1 [ Heading.Props [ Style [ Margin "2rem" ] ] ] [ str "Address" ]
+            Heading.h3 [ Heading.IsSubtitle ] [ str (sprintf "Building No: %d" model.BuildingNo) ]
+            Heading.h3 [ Heading.IsSubtitle ] [ str (sprintf "Street: %s" model.Street) ]
+            Heading.h3 [ Heading.IsSubtitle ] [ str (sprintf "City: %s" model.City) ]
+            Heading.h3 [ Heading.IsSubtitle ] [ str (sprintf "Postcode: %s" model.Postcode) ]
         ]
 
 type Page =
@@ -59,43 +74,81 @@ type Model =
       CurrentPage: Page
       SubModel: SubModel }
 
-type UpdatePersonName = UpdatePersonName of string
+type Msg = PersonNameChanged of string
 
-let init page : Model * Cmd<UpdatePersonName> =
+let init page : Model * Cmd<Msg> =
     let page = page |> Option.defaultValue HomePage
 
     let subModel =
         match page with
-        | HomePage -> HomePageModel (HomePage.init ())
-        | AddressPage -> AddressPageModel (AddressPage.init ())
-        | PersonPage name -> PersonPageModel (PersonPage.init name)
+        | HomePage -> HomePageModel (HomePage.init())
+        | AddressPage -> AddressPageModel (AddressPage.init())
+        | PersonPage name -> PersonPageModel (name |> PersonPage.init)
 
     { CurrentPage = page
       SubModel = subModel
       NameEntry = "" }, Cmd.none
 
-let update msg model =
+let update (msg : Msg) (model : Model) : Model * Cmd<Msg> =
     match msg with
-    | UpdatePersonName name -> { model with NameEntry = name }, Cmd.none
+    | PersonNameChanged name -> { model with NameEntry = name }, Cmd.none
 
-let view (model : Model) (dispatch : UpdatePersonName -> unit) =
-    Container.container [] [
-        Label.label [] [ str "Name" ]
-        Input.text [
-            Input.Value model.NameEntry
-            Input.Props [ OnChange (fun ev -> dispatch (UpdatePersonName !!ev.target?value)) ]
+let button txt href options =
+    Button.a
+        [ Button.Color IsPrimary
+          Button.Props [ Href href ]
+          yield! options ]
+        [ str txt ]
+
+let view (model : Model) (dispatch : Msg -> unit) =
+    let navigationButton text href page model =
+        button text href [
+            Button.IsFullWidth
+            if model.CurrentPage = page then Button.Color IsSuccess
         ]
 
-        let href = sprintf "#person/%s" model.NameEntry
+    div [] [
+        Navbar.navbar [ Navbar.Color IsPrimary ] [
+            Navbar.Item.div [ ] [
+                Heading.h1 [ ] [ str "Elmish navigation example" ]
+            ]
+        ]
 
-        Button.a [ Button.Props [ Href href ] ] [ str "Person Page" ]
-        Button.a [ Button.Props [ Href "#home" ] ] [ str "Home Page" ]
-        Button.a [ Button.Props [ Href "#address" ] ] [ str "Address Page" ]
+        Content.content [] [
+            Columns.columns [ Columns.IsCentered ] [
+                Column.column [ Column.Width (Screen.All, Column.Is3) ] [
+                    Control.div [ Control.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [
+                        br []
+                        Label.label [] [ str "Name" ]
+                        Input.text [
+                            Input.Value model.NameEntry
+                            Input.Placeholder "Enter Name Here..."
+                            Input.Props [ OnChange (fun ev -> dispatch (PersonNameChanged !!ev.target?value)) ]
+                        ]
+                    ]
+                    Control.div [ Control.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [
+                        br []
+                        let href = sprintf "#person/%s" model.NameEntry
+                        button (sprintf "Set URL to '%s'" href) href [ ]
+                    ]
+                ]
+            ]
+        ]
 
-        match model.SubModel with
-        | HomePageModel m -> HomePage.view m dispatch
-        | AddressPageModel m -> AddressPage.view m dispatch
-        | PersonPageModel m -> PersonPage.view m dispatch
+        Container.container [ ] [
+            Columns.columns [ Columns.Option.Props [ Style  [ Margin "3em" ] ] ] [
+                Column.column [] [ navigationButton "HOME PAGE" "#home" HomePage model ]
+                Column.column [] [ navigationButton "ADDRESS PAGE" "#address" AddressPage model ]
+            ]
+            Columns.columns [] [
+                Column.column [] [
+                    match model.SubModel with
+                    | HomePageModel m -> HomePage.view m dispatch
+                    | AddressPageModel m -> AddressPage.view m dispatch
+                    | PersonPageModel m -> PersonPage.view m dispatch
+                ]
+            ]
+        ]
     ]
 
 module Navigation =
@@ -110,10 +163,25 @@ module Navigation =
 
     let urlUpdate (page: Page option) _ =
         let page = page |> Option.defaultValue HomePage
+
         let model, _ = Some page |> init
-        { model with CurrentPage = page }, Cmd.none
+
+        { model with
+            CurrentPage = page;
+            SubModel = model.SubModel }, Cmd.none
+
+#if DEBUG
+open Elmish.Debug
+open Elmish.HMR
+#endif
 
 Program.mkProgram init update view
 |> Program.toNavigable (UrlParser.parseHash Navigation.pageParser) Navigation.urlUpdate
+#if DEBUG
+|> Program.withConsoleTrace
+#endif
 |> Program.withReactBatched "elmish-app"
+#if DEBUG
+|> Program.withDebugger
+#endif
 |> Program.run
